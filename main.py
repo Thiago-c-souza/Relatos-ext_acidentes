f1785dc8680f1f16f22802eace213077cf46e516
thiago <thiagocaixetadesouza10@gmail.com>
2025-01-08 11:27:01 -0300

    Feito a segunda parte do formulario de relatos

--- main.py
+++ main.py
@@ -20,5 +20,18 @@ def main():
         Risco = input("O ato inseguro representava um risco imediato?\n")
         Impacto = input("Qual foi o impacto do ato inseguro?\n")
         Descricao = input("Descrição do ocorrido:\n")
+    elif TipoRelato == "Condição Insegura":
+        Data = input("Data do ocorrido:\n")
+        Local = input("Local do ocorrido:\n")
+        Responsavel = input("Quem foi o responsável pelo ato ou condição?\n")
+        Observado = input("O relato foi observado diretamente ou informado por terceiros?\n")
+        Qual = input("Qual foi a condição insegura observada?\n")
+        Antes = input("Essa condição já havia sido relatada anteriormente?\n")
+        Risco = input("Qual é o nível de risco da condição insegura?\n")
+        Incidente = input("Houve algum incidente associado a essa condição insegura?\n")
+        Descricao = input("Descrição do ocorrido:\n")
+    else:
+        print("Você digitou um tipo de relato invalido.\nTipo Validos:  'Ato Inseguro' e 'Condição Insegura'\nTente novamente...")
+        main()
 
 main()
